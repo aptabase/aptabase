53e184fa251b89bd17263bf12a9b3b3eee12709d
goenning <me@goenning.net>
2023-06-06 10:19:45 +0100

    new icons and dark mode support for ios icon

--- webapp/pkg/widgets/os.tsx
+++ webapp/pkg/widgets/os.tsx
@@ -1,16 +1,11 @@
-const fallbackImageUrl = new URL(`./icons/os/default.svg`, import.meta.url).href;
+const fallbackImageUrl = new URL(`./icons/os/default.svg`, import.meta.url);
 
 export function getOperatingSystemImageUrl(name: string) {
   const lcName = name.toLowerCase().replaceAll(/[^a-z]*/g, "");
-  const png = new URL(`./icons/os/${lcName}.png`, import.meta.url);
-  if (png.pathname !== "/undefined") {
-    return png.href;
-  }
-
   const svg = new URL(`./icons/os/${lcName}.svg`, import.meta.url);
   if (svg.pathname !== "/undefined") {
     return svg.href;
   }
 
-  return fallbackImageUrl;
+  return fallbackImageUrl.href;
 }
