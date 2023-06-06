6d0451a109826e9d87ebb22789b483f402f78fc4
goenning <me@goenning.net>
2023-06-06 10:51:23 +0100

    increase debug z-index

--- webapp/src/routes/app/components/DebugModeBanner.tsx
+++ webapp/src/routes/app/components/DebugModeBanner.tsx
@@ -1,6 +1,6 @@
 export function DebugModeBanner() {
   return (
-    <div className="fixed top-12 lg:top-0 left-0 lg:left-64 right-0 mb-4">
+    <div className="fixed top-12 z-20 lg:top-0 left-0 lg:left-64 right-0 mb-4">
       <div className="w-full border-t-4 border-t-warning text-center text-xs">
         <div className="inline-block -mt-2 text-white px-2 rounded-b-md bg-warning">
           <span>Debug Data</span>
