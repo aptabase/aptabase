6eed6df94e3bf464f9d91590e45637a0560cb4e2
goenning <me@goenning.net>
2023-07-17 09:43:23 +0100

    fix warning

--- src/Controllers/BillingController.cs
+++ src/Controllers/BillingController.cs
@@ -33,7 +33,7 @@ public class BillingController : Controller
         var debugAppIds = releaseAppIds.Select(id => $"{id}_DEBUG");
         var appIds = releaseAppIds.Concat(debugAppIds);
 
-        var usage = await _queryClient.NamedQuerySingleAsync<BillingUsage>("get_billing_usage", new KeyValuePair<string, string?>[] {
+        var usage = await _queryClient.NamedQuerySingleAsync<BillingUsage>("get_billing_usage", new KeyValuePair<string, string>[] {
             new("app_ids", String.Join(",", appIds)),
             new("year", DateTime.UtcNow.Year.ToString()),
             new("month", DateTime.UtcNow.Month.ToString())
