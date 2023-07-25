1d8cda476ca1c6dc7f8b65a7931161e77749a663
goenning <me@goenning.net>
2023-07-25 09:50:09 +0100

    also catch BadHttpRequestException

--- src/Extensions/ExceptionMiddleware.cs
+++ src/Extensions/ExceptionMiddleware.cs
@@ -1,3 +1,5 @@
+// Custom Exception Middleware to catch common exceptions caused by client disconnects
+// Can be removed on .NET 8 because of https://github.com/dotnet/aspnetcore/pull/46330
 public class ExceptionMiddleware
 {
     private readonly RequestDelegate _next;
@@ -19,7 +21,15 @@ public class ExceptionMiddleware
         {
             _logger.LogWarning("Request to {Path} was cancelled.", context.Request.Path.Value);
 
-            context.Response.StatusCode = 418; // I'm a teapot
+            if (!context.Response.HasStarted)
+                context.Response.StatusCode = 418; // I'm a teapot
+        }
+        catch (BadHttpRequestException) when (context.RequestAborted.IsCancellationRequested)
+        {
+            _logger.LogWarning("Request to {Path} was cancelled.", context.Request.Path.Value);
+
+            if (!context.Response.HasStarted)
+                context.Response.StatusCode = 418; // I'm a teapot
         }
     }
 }
