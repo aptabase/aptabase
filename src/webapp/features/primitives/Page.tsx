cf53e88e83880cdc1bd8e76975bdd79a8d277a10
goenning <me@goenning.net>
2023-09-05 22:00:13 +0100

    lazy load motion

--- src/webapp/features/primitives/Page.tsx
+++ src/webapp/features/primitives/Page.tsx
@@ -1,6 +1,6 @@
-import { motion } from "framer-motion";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { LazyMotion, domAnimation, m } from "framer-motion";
 
 type Props = {
   title: string;
@@ -15,14 +15,16 @@ export function Page(props: Props) {
   }, [props.title]);
 
   return (
-    <motion.div
-      key={location.pathname}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.7 }}
-    >
-      {props.children}
-    </motion.div>
+    <LazyMotion features={domAnimation}>
+      <m.div
+        key={location.pathname}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.7 }}
+      >
+        {props.children}
+      </m.div>
+    </LazyMotion>
   );
 }
