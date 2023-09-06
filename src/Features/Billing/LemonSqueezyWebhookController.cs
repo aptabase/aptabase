9eddb8cc514f23d2981bfb1c231ffb475491682c
goenning <me@goenning.net>
2023-09-06 21:09:56 +0100

    remove lock on subscribe

--- src/Features/Billing/LemonSqueezyWebhookController.cs
+++ src/Features/Billing/LemonSqueezyWebhookController.cs
@@ -78,6 +78,7 @@ public class LemonSqueezyWebhookController : Controller
             return BadRequest(new { message = "Missing 'user_id' on meta.custom_data" });
         }
 
+        await _db.Connection.ExecuteAsync(@"UPDATE users SET lock_reason = null WHERE id = @ownerId", new { ownerId });
         await _db.Connection.ExecuteAsync(@"INSERT INTO subscriptions
                                                 (id, owner_id, customer_id, product_id, variant_id, status, ends_at)
                                             VALUES
