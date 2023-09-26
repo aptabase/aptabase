346f64dc407727f02c845fc4a2255ce3f1621165
goenning <me@goenning.net>
2023-09-26 09:04:52 +0100

    add a temporary headers controller

---
+++ src/Features/Headers/HeadersController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Aptabase.Features.GeoIP;
+
+namespace Aptabase.Features.Headers;
+
+[ApiController]
+[ResponseCache(NoStore = true, Location = ResponseCacheLocation.None)]
+public class HeadersController : Controller
+{
+    private readonly IGeoIPClient _geoIP;
+
+    public HeadersController(IGeoIPClient geoIP)
+    {
+        _geoIP = geoIP ?? throw new ArgumentNullException(nameof(geoIP));
+    }
+
+    [HttpGet("/api/headers")]
+    public IActionResult Headers()
+    {
+        var location = _geoIP.GetClientLocation(HttpContext);
+        var ipAddress = HttpContext.ResolveClientIpAddress();
+        var headers = HttpContext.Request.Headers.ToDictionary(x => x.Key, x => x.Value.ToString());
+
+        return Ok(new {
+            ipAddress,
+            location,
+            headers
+        });
+    }
+}
