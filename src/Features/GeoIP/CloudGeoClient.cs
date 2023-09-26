68e0bc12ad08065e289041c4f0040c9311e71c0a
goenning <me@goenning.net>
2023-09-26 10:19:59 +0100

    add support for standard geo headers

--- src/Features/GeoIP/CloudGeoClient.cs
+++ src/Features/GeoIP/CloudGeoClient.cs
@@ -1,16 +1,43 @@
+using System.Text.Json;
+
 namespace Aptabase.Features.GeoIP;
 
 public class CloudGeoClient : IGeoIPClient
 {
+    private readonly Dictionary<string, string> _regions;
+
+    public CloudGeoClient(EnvSettings env)
+    {
+        var text = File.ReadAllText(Path.Combine(env.EtcDirectoryPath, "geoip/iso3166-2.json"));
+        var regions = JsonSerializer.Deserialize<Dictionary<string, string>>(text);
+        if (regions == null)
+            throw new Exception("Failed to deserialize geoip/iso3166-2.json");
+
+        _regions = regions;
+    }
+
     public GeoLocation GetClientLocation(HttpContext httpContext)
     {
-        var countryCode = httpContext.Request.Headers["CloudFront-Viewer-Country"].ToString();
-        var regionName = httpContext.Request.Headers["CloudFront-Viewer-Country-Region-Name"].ToString();
+        var countryCode = GetHeader(httpContext, "cdn-requestcountrycode", "CloudFront-Viewer-Country");
+        var regionCode = GetHeader(httpContext, "cdn-requeststatecode", "Cloudfront-Viewer-Country-Region");
+        var regionName = _regions.TryGetValue($"{countryCode}-{regionCode}", out var name) ? name : "";
 
         return new GeoLocation
         {
-            CountryCode = countryCode?.ToUpper() ?? "",
-            RegionName = Uri.UnescapeDataString(regionName ?? "")
+            CountryCode = countryCode,
+            RegionName = regionName
         };
+    }
+
+    private string GetHeader(HttpContext httpContext, params string[] names)
+    {
+        foreach (var name in names)
+        {
+            var value = httpContext.Request.Headers[name].ToString()?.ToUpper() ?? "";
+            if (!string.IsNullOrEmpty(value))
+                return value;
+        }
+
+        return "";
     }
 }
