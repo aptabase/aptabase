6823ea32721014ef763010f10ad85d1058bd21ab
goenning <me@goenning.net>
2023-10-16 12:59:11 +0100

    parse dates too

--- src/Features/Stats/TinybirdDateTimeJsonConverter.cs
+++ src/Features/Stats/TinybirdDateTimeJsonConverter.cs
@@ -6,13 +6,22 @@ namespace Aptabase.Features.Stats;
 
 public class TinybirdDateTimeJsonConverter : JsonConverter<DateTime>
 {
+    private readonly static string[] FORMATS = new[]
+    {
+        "yyyy-MM-dd HH:mm:ss",
+        "yyyy-MM-dd"
+    };
+
     public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         var input = reader.GetString() ?? string.Empty;
-        if (DateTime.TryParseExact(input, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal, out var dateTime))
-            return dateTime.ToUniversalTime();
+        foreach (var format in FORMATS) 
+        {
+            if (DateTime.TryParseExact(input, format, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal, out var dateTime))
+                return dateTime.ToUniversalTime();
+        }
 
-        throw new NotSupportedException($"Could not parse {input} to DateTime. Value must be in yyyy-MM-dd HH:mm:ss format.");
+        throw new NotSupportedException($"Could not parse {input} to DateTime.");
     }
 
     public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
