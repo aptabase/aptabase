d20e5c022b0b67d80585c7d91d1b334fd0b746a3
goenning <me@goenning.net>
2023-11-12 12:31:03 +0000

    remove appbuildnumber widget

--- src/webapp/features/analytics/dashboard/VersionWidget.tsx
+++ src/webapp/features/analytics/dashboard/VersionWidget.tsx
@@ -9,26 +9,6 @@ type Props = {
 };
 
 export function VersionWidget(props: Props) {
-  const [searchParams] = useSearchParams();
-  const appVersion = searchParams.get("appVersion") || "";
-
-  if (appVersion) {
-    return (
-      <TopNDataContainer appId={props.appId} queryName="top-appbuildnumbers" query={topAppBuildNumbers}>
-        {(data) => (
-          <TopNChart
-            {...data}
-            id="appbuildnumbers"
-            key="appbuildnumbers"
-            title={<TopNTitle backProperty="appVersion">{appVersion}</TopNTitle>}
-            defaultFormat="percentage"
-            valueLabel="Sessions"
-          />
-        )}
-      </TopNDataContainer>
-    );
-  }
-
   return (
     <TopNDataContainer appId={props.appId} queryName="top-appversions" query={topAppVersions}>
       {(data) => (
@@ -37,7 +17,6 @@ export function VersionWidget(props: Props) {
           id="appversions"
           key="appversions"
           title="App Versions"
-          searchParamKey="appVersion"
           defaultFormat="percentage"
           valueLabel="Sessions"
         />
