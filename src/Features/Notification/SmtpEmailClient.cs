c33b92ac0290be78381905b7bc7a9d30ec692a22
goenning <me@goenning.net>
2023-11-25 20:05:47 +0000

    use mailkit

--- src/Features/Notification/SmtpEmailClient.cs
+++ src/Features/Notification/SmtpEmailClient.cs
@@ -1,33 +1,35 @@
-using System.Net;
-using System.Net.Mail;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace Aptabase.Features.Notification;
 
 public class SmtpEmailClient : IEmailClient
 {
     private readonly EnvSettings _env;
-    private readonly SmtpClient _smtp;
     private readonly TemplateEngine _engine = new();
 
     public SmtpEmailClient(EnvSettings env)
     {
         _env = env ?? throw new ArgumentNullException(nameof(env));
-        _smtp = new SmtpClient(env.SmtpHost, env.SmtpPort);
-        _smtp.EnableSsl = IsTLSPort(env.SmtpPort);
-
-        if (!string.IsNullOrEmpty(env.SmtpUsername))
-            _smtp.Credentials = new NetworkCredential(env.SmtpUsername, env.SmtpPassword);
     }
 
     public async Task SendEmailAsync(string to, string subject, string templateName, Dictionary<string, string>? properties, CancellationToken cancellationToken)
     {
+        using var smtp = new SmtpClient();
+        await smtp.ConnectAsync(_env.SmtpHost, _env.SmtpPort, IsTLSPort(_env.SmtpPort), cancellationToken);
+
+        if (!string.IsNullOrEmpty(_env.SmtpUsername))
+            await smtp.AuthenticateAsync(_env.SmtpUsername, _env.SmtpPassword, cancellationToken);
+
         var body = await _engine.Render(templateName, subject, properties);
-        var msg = new MailMessage(_env.SmtpFromAddress, to, subject, body)
-        {
-            IsBodyHtml = true
-        };
-        await _smtp.SendMailAsync(msg, cancellationToken);
+        var msg = new MimeMessage();
+        msg.From.Add(new MailboxAddress("", _env.SmtpFromAddress));
+        msg.To.Add(new MailboxAddress("", to));
+        msg.Subject = subject;
+        msg.Body = new TextPart("html") { Text = body };
+        await smtp.SendAsync(msg, cancellationToken);
+        await smtp.DisconnectAsync(true, cancellationToken);
     }
 
-    private bool IsTLSPort(int port) => port == 587 || port == 465;
+    private static bool IsTLSPort(int port) => port == 587 || port == 465;
 }
