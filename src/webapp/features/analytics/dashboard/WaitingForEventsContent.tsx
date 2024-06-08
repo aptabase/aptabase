f0d3900a6dcc7963b7919948eca9244bbb752891
Bogdan Ivanov <bogdan.ivanov@uipath.com>
2024-06-08 15:45:17 +0300

    Add app key in initial 'wait for events' content

--- src/webapp/features/analytics/dashboard/WaitingForEventsContent.tsx
+++ src/webapp/features/analytics/dashboard/WaitingForEventsContent.tsx
@@ -2,15 +2,45 @@ import { Link } from "react-router-dom";
 import { isSupportEnabled } from "@features/env";
 import { ContactUsLink } from "@features/support";
 import { PingSignal } from "@components/PingSignal";
+import { useCurrentApp } from "@features/apps";
+import { IconCopy, IconCheck } from "@tabler/icons-react";
+import { useState } from "react";
 
 type Props = {
   appId: string;
 };
 
 export function WaitingForEventsContent(props: Props) {
+  const app = useCurrentApp();
+  const [justCopied, setJustCopied] = useState(false);
+
   return (
     <>
       <p className="text-center font-semibold">Waiting for the first event...</p>
+
+      {app && (
+        <div className="text-center">
+          <p className="text-muted-foreground text-sm mb-1">
+            App Key for <span className="text-foreground">{app.name}</span>
+          </p>
+          <div className="flex items-center mb-2 gap-2">
+            <span className="font-medium text-xl">{app.appKey}</span>
+            {!justCopied && (
+              <IconCopy
+                className="cursor-pointer hover:text-muted-foreground transition-colors duration-200 ease-in-out"
+                stroke={2}
+                onClick={() => {
+                  setJustCopied(true);
+                  setTimeout(() => setJustCopied(false), 2000);
+                  navigator.clipboard.writeText(app.appKey);
+                }}
+              />
+            )}
+
+            {justCopied && <IconCheck stroke={2} />}
+          </div>
+        </div>
+      )}
       <p className="text-center text-muted-foreground mx-14">
         Install an{" "}
         <Link to={`/${props.appId}/instructions`} className="text-foreground underline">
