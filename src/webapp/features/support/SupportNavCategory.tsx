f089c7511bdd27014d5131b4bd13027435d0862c
Bogdan Ivanov <bogdan.ivanov@uipath.com>
2024-06-24 23:50:29 +0300

    fix: merge help & feedback

--- src/webapp/features/support/SupportNavCategory.tsx
+++ src/webapp/features/support/SupportNavCategory.tsx
@@ -1,14 +1,13 @@
+import { IconGift, IconMessageCircle } from "@tabler/icons-react";
 import { NavCategory, NavItem } from "../navigation";
 import { useSupport } from "./useSupport";
-import { IconGift, IconHelpCircle, IconMessageCircle } from "@tabler/icons-react";
 
 export function SupportNavCategory() {
   const { toggleChat } = useSupport();
 
   return (
     <NavCategory title="Product">
-      <NavItem label="Help" onClick={toggleChat} icon={IconHelpCircle} />
-      <NavItem label="Feedback" onClick={toggleChat} icon={IconMessageCircle} />
+      <NavItem label="Help & Feedback" onClick={toggleChat} icon={IconMessageCircle} />
       <NavItem label="Affiliates" href="https://aptabase.lemonsqueezy.com/affiliates" icon={IconGift} />
     </NavCategory>
   );
